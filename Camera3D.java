f0eb7f2f4e86a8bf1092461456371725170ad72a
Toby <tobik.brnak@icloud.com>
2023-02-06 11:21:13 +0100

    rozdeleni good, ted list buttons

--- Camera3D.java
+++ Camera3D.java
@@ -3,17 +3,16 @@ package com.example.demo2;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -36,6 +35,12 @@ public class Camera3D extends Application {
     private final DoubleProperty angleX = new SimpleDoubleProperty(0);
     private final DoubleProperty angleY = new SimpleDoubleProperty(0);
 
+    Group gp = new Group();
+
+    BorderPane bp = new BorderPane(gp);
+
+    SmartGroup sg [] = {new SmartGroup(), new SmartGroup()};
+
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -44,9 +49,11 @@ public class Camera3D extends Application {
         s1.translateXProperty().set(200);
         s1.translateZProperty().set(-200);
 
+        Button bt [] = {new Button(), new Button()};
+
         Box box = prepareBox();
 
-
+/*
         Camera cm = new PerspectiveCamera(true);
 
         cm.translateXProperty().set(100);
@@ -57,38 +64,68 @@ public class Camera3D extends Application {
         cm.setNearClip(1);
         cm.setFarClip(1500);
 
+ */
 
 
-        SmartGroup sg [] = {new SmartGroup(), new SmartGroup()};
-        sg[0].getChildren().addAll(cm);
+
+        // bylo SmartGroup sg [] = {new SmartGroup(), new SmartGroup()};
+        //sg[0].getChildren().addAll(cm);
         sg[1].getChildren().addAll(s1, box);
 
 
-        Group gp = new Group();
-        gp.getChildren().addAll(sg[0], sg[1]);
+        //tady bylo Group gp = new Group();
+        gp.getChildren().addAll(sg[1]);
 
 
-        Button bt = new Button("Kurwa");
-        bt.setOnAction(actionEvent -> {
+        ScrollPane sp = new ScrollPane();
+
+        bt[0].setText("Box");
+        bt[0].setOnAction(actionEvent -> {
 
             Box b = prepareBox();
-            sg[0].getChildren().addAll(b);
+            gp.getChildren().addAll(b);
         });
-        BorderPane bp = new BorderPane(gp);
-        bp.setLeft(bt);
+
+        bt[1].setText("BoxWOW");
+        bt[1].setOnAction(actionEvent -> {
+
+            Box b = prepareBox();
+            gp.getChildren().addAll(b);
+        });
+
+        sp.setContent(bt[0]);
+        sp.setContent(bt[1]);
+
+
+
+        //HBox h = new HBox(vb, sp);
+        //vb.relocate(500, 500);
+        //sp.relocate(1500, 500);
+
+        //Pane p = new Pane();
+        //p.getChildren().addAll(gp);
+
+        //Tady bylo BorderPane bp = new BorderPane(gp);
+        bp.setCenter(gp);
+        bp.setLeft(sp);
+
+
+
+
+
 
 
 
         Scene scene = new Scene(bp, WIDTH, HEIGHT);
         scene.setFill(Color.SILVER);
-        scene.setCamera(cm);
+        //scene.setCamera(cm);
 
 
 
-        initMouseControl( sg[1] , scene, stage);
+        initMouseControl( sg[1] , scene, stage, bp);
 
 
-
+/*
         stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch (event.getCode()){
                 case W: {
@@ -142,6 +179,8 @@ public class Camera3D extends Application {
             }
         });
 
+ */
+
         stage.setTitle("ArchitectureAPP");
         stage.setScene(scene);
         stage.show();
@@ -166,7 +205,7 @@ public class Camera3D extends Application {
     }
 
 
-    private void initMouseControl(SmartGroup sg, Scene scene, Stage stage) {
+    private void initMouseControl(SmartGroup sg, Scene scene, Stage stage, BorderPane bp) {
 
         Rotate xRotate;
         Rotate yRotate;
@@ -177,7 +216,7 @@ public class Camera3D extends Application {
         xRotate.angleProperty().bind(angleX);
         yRotate.angleProperty().bind(angleY);
 
-        scene.setOnMousePressed(event -> {
+        bp.setOnMousePressed(event -> {
 
             anchorX = event.getSceneX();
             anchorY = event.getSceneY();
@@ -185,15 +224,23 @@ public class Camera3D extends Application {
             anchorAngleY = angleY.get();
 
         });
-        scene.setOnMouseDragged(event -> {
+        bp.setOnMouseDragged(event -> {
             angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
             angleY.set(anchorAngleY + (anchorX - event.getSceneX()));
         });
 
-        scene.addEventHandler(ScrollEvent.SCROLL, event ->{
+        bp.addEventHandler(ScrollEvent.SCROLL, event ->{
             double delta = event.getDeltaY();
             sg.translateZProperty().set(sg.getTranslateZ() - delta);
 
+        });
+
+        bp.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            switch (event.getCode()){
+                case UP: {
+                    sg.translateXProperty().set(sg.getTranslateX() + 10);
+                }
+            }
         });
     }
 
