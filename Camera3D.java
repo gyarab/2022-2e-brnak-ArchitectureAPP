9e07cc6084c0c2a4eb0ce4ef57ea29d1866a039e
Toby <tobik.brnak@icloud.com>
2023-02-23 17:53:04 +0100

    Samostatne funtguje jen u Sphere ale kmo tohle uz je na odevzdani hochu kotlar jo

--- Camera3D.java
+++ Camera3D.java
@@ -3,28 +3,32 @@ package com.example.demo2;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.geometry.Pos;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Point3D;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
-import javafx.scene.shape.Sphere;
+import javafx.scene.shape.*;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Camera3D extends Application {
+
+    Scanner sc = new Scanner(System.in);
 
     private static final float WIDTH = 2000;
     private static final float HEIGHT = 1000;
@@ -35,23 +39,90 @@ public class Camera3D extends Application {
     private final DoubleProperty angleX = new SimpleDoubleProperty(0);
     private final DoubleProperty angleY = new SimpleDoubleProperty(0);
 
-    Group gp = new Group();
+    //Group gp = new Group();
 
-    BorderPane bp = new BorderPane(gp);
+    int pomocnahodnota = 0;
 
-    SmartGroup sg [] = {new SmartGroup(), new SmartGroup()};
+    int Sphere = 1;
+    int [] zaznamSphere = new int[1000];
+    int Box = 0;
+    int Cube = 0;
+    int Cylindr = 0;
+
+    int pocetobjektu = 0;
+
+    int k = 0;
+    int [] cisla = new int[1000];
+
+    ArrayList <Sphere> spe = new ArrayList();
+    ArrayList <Box> b1 = new ArrayList();
+    ArrayList <Box> b2 = new ArrayList();
+    ArrayList <Cylinder> cil = new ArrayList<>();
+
+    int vybrano = 1;
+
+
+    SmartGroup sg  = new SmartGroup();
+
+    Button bt [] = {new Button(), new Button(), new Button(), new Button()};
+    Button btplusminus [] = {new Button(), new Button()};
+    Button xyz [] = {new Button("X"), new Button("Y"), new Button("Z")};
+    Button start = new Button("Start");
+
+    PhongMaterial materials [] = {new PhongMaterial(), new PhongMaterial(), new PhongMaterial()};
+
+
+    ChoiceBox chb [] = {new ChoiceBox(), new ChoiceBox()};
+
+
+    ScrollPane sp = new ScrollPane();
+
+    VBox v = new VBox();
+
+    BorderPane bp = new BorderPane(sg);
+
+    //Scene uvodscene = new Scene();
+    Scene hlscene = new Scene(bp, WIDTH, HEIGHT);
+    //Scene datascene =  new Scene(lb);
 
 
     @Override
     public void start(Stage stage) throws IOException {
 
-        Sphere s1 = prepareSphere();
-        s1.translateXProperty().set(200);
-        s1.translateZProperty().set(-200);
 
-        Button bt [] = {new Button(), new Button()};
 
-        Box box = prepareBox();
+
+        materials[0].setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/mramor.png")));
+        materials[1].setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/orechdrevo-3.jpg")));
+        materials[2].setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/ffffff.png")));
+
+        chb[0].getItems().add("Mramor");
+        chb[0].getItems().add("Ořechové dřeevo");
+        chb[0].getItems().add("Bílá barva");
+
+
+
+        Box lineX = new Box(100, 2, 2);
+        PhongMaterial material1 = new PhongMaterial();
+        material1.setDiffuseColor(Color.BLUE);
+        lineX.setMaterial(material1);
+        lineX.translateXProperty().set(50);
+
+
+        Box lineY = new Box(2, 100, 2);
+        PhongMaterial material2 = new PhongMaterial();
+        material2.setDiffuseColor(Color.YELLOW);
+        lineY.setMaterial(material2);
+        lineY.translateYProperty().set(50);
+
+        Box lineZ = new Box(2,2,100);
+        PhongMaterial material3 = new PhongMaterial();
+        material3.setDiffuseColor(Color.RED);
+        lineZ.setMaterial(material3);
+        lineZ.translateZProperty().set(50);
+
+
+
 
 /*
         Camera cm = new PerspectiveCamera(true);
@@ -68,33 +139,66 @@ public class Camera3D extends Application {
 
 
 
+
+
+
+
         // bylo SmartGroup sg [] = {new SmartGroup(), new SmartGroup()};
         //sg[0].getChildren().addAll(cm);
-        sg[1].getChildren().addAll(s1, box);
+        sg.getChildren().addAll(lineX, lineY, lineZ);
 
 
         //tady bylo Group gp = new Group();
-        gp.getChildren().addAll(sg[1]);
+        // gp.getChildren().addAll(sg);
 
 
-        ScrollPane sp = new ScrollPane();
 
         bt[0].setText("Box");
+        bt[0].setPrefSize(200,30);
+        bt[0].relocate(0,0);
         bt[0].setOnAction(actionEvent -> {
 
             Box b = prepareBox();
-            gp.getChildren().addAll(b);
+            sg.getChildren().addAll(b);
         });
 
-        bt[1].setText("BoxWOW");
+        bt[1].setText("Cube");
+        bt[1].setPrefSize(200,30);
+        bt[1].relocate(0,100);
         bt[1].setOnAction(actionEvent -> {
 
-            Box b = prepareBox();
-            gp.getChildren().addAll(b);
+            Box c = prepareCube();
+            sg.getChildren().addAll(c);
         });
 
-        sp.setContent(bt[0]);
-        sp.setContent(bt[1]);
+        bt[2].setText("Sphere");
+        bt[2].setPrefSize(200,30);
+        bt[2].relocate(0,200);
+        bt[2].setOnAction(actionEvent -> {
+
+            Sphere s = prepareSphere();
+            sg.getChildren().addAll(s);
+        });
+
+        bt[3].setText("Cylinder");
+        bt[3].setPrefSize(200,30);
+        bt[3].relocate(0,300);
+        bt[3].setOnAction(actionEvent -> {
+
+            Cylinder c = prepareCylinder();
+            sg.getChildren().addAll(c);
+        });
+
+
+
+
+        sp.setPrefSize(200,1000);
+        v.setPrefSize(200, 1500);
+        sp.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+        sp.setContent(v);
+
+        v.getChildren().addAll(bt[0], bt[1], bt[2], bt[3], chb[0], chb[1]);
+
 
 
 
@@ -106,9 +210,12 @@ public class Camera3D extends Application {
         //p.getChildren().addAll(gp);
 
         //Tady bylo BorderPane bp = new BorderPane(gp);
-        bp.setCenter(gp);
+        bp.setCenter(sg);
         bp.setLeft(sp);
 
+        v.setBackground(new Background(new BackgroundFill(Color.GRAY, new CornerRadii(0), Insets.EMPTY)));
+        sp.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, new CornerRadii(0), Insets.EMPTY)));
+        bp.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, new CornerRadii(0), Insets.EMPTY)));
 
 
 
@@ -116,17 +223,15 @@ public class Camera3D extends Application {
 
 
 
-        Scene scene = new Scene(bp, WIDTH, HEIGHT);
-        scene.setFill(Color.SILVER);
-        //scene.setCamera(cm);
+        hlscene.setFill(Color.SILVER);
 
 
 
-        initMouseControl( sg[1] , scene, stage, bp);
+        initMouseControl( sg , hlscene, stage, bp, v);
 
 
 /*
-        stage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+        sg.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch (event.getCode()){
                 case W: {
                     cm.translateZProperty().set(cm.getTranslateZ() + 10);
@@ -180,32 +285,182 @@ public class Camera3D extends Application {
         });
 
  */
+        chb[1].setPrefSize(200,30);
+        chb[1].relocate(0,400);
+        chb[1].getSelectionModel().selectedIndexProperty().addListener(
+                (ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+                    vybrano = new_val.intValue();
+                });
+
+        chb[0].setPrefSize(200,30);
+        chb[0].relocate(0,500);
+        chb[0].getSelectionModel().selectedIndexProperty().addListener(
+                (ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+                    if (cisla[vybrano] == 1) {
+                        pomocnahodnota = Sphere;
+                        for (int i = 0; i <= Sphere; i++) {
+                            if (zaznamSphere[vybrano] == zaznamSphere[i]){
+                                break;
+                            } else {
+                                pomocnahodnota--;
+                            }
+                        }
+                        spe.get(Sphere - pomocnahodnota).setMaterial(materials[new_val.intValue()]);
+                    } else if (cisla[vybrano] == 2) {
+                        b1.get(Box).setMaterial(materials[new_val.intValue()]);
+                    } else if (cisla[vybrano] == 3) {
+                        b2.get(Cube).setMaterial(materials[new_val.intValue()]);
+                    }else if (cisla[vybrano] == 4) {
+                        cil.get(Cylindr).setMaterial(materials[new_val.intValue()]);
+                    }
+
+                });
+
+
 
         stage.setTitle("ArchitectureAPP");
-        stage.setScene(scene);
+        stage.setScene(hlscene);
         stage.show();
+
+        //Infoscene
+
+        Label lb = new Label("ArchitectureAPP");
+
+
+
+
+
+
     }
 
     private Sphere prepareSphere() {
+        Sphere++;
+        System.out.println("PIS");
+        int v = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        String s = sc.next();
         PhongMaterial material = new PhongMaterial();
-        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/mramor.png")));
-        Sphere sp = new Sphere(80);
-        sp.setMaterial(material);
-        return sp;
+        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/ffffff.png")));
+        spe.add(new Sphere(v));
+        spe.get(Sphere-2).setMaterial(material);
+
+        spe.get(Sphere-2).translateXProperty().set(x);
+        spe.get(Sphere-2).translateYProperty().set(y);
+        spe.get(Sphere-2).translateZProperty().set(z);
+
+        chb[1].getItems().add(s);
+
+
+        cisla[k] = 1;
+        k++;
+        zaznamSphere[Sphere - 2] = pocetobjektu;
+        pocetobjektu++;
+
+        return spe.get(Sphere-2);
     }
 
     private Box prepareBox() {
+        Box++;
+        System.out.println("PIS");
+        int x1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int z3 = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        String s = sc.next();
+
         PhongMaterial material = new PhongMaterial();
         //material.setDiffuseColor(Color.MIDNIGHTBLUE);
-        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/mramor.png")));
-        Box b1 = new Box(80,50, 20);
-        b1.setMaterial(material);
-        b1.translateZProperty().set(-200);
-        return b1;
+        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/ffffff.png")));
+
+
+        b1.add(new Box(x1,y2,z3));
+        b1.get(Box-1).setMaterial(material);
+
+        b1.get(Box-1).translateXProperty().set(x);
+        b1.get(Box-1).translateYProperty().set(y);
+        b1.get(Box-1).translateZProperty().set(z);
+
+
+        chb[1].getItems().add(s);
+
+
+        cisla[k] = 2;
+        k++;
+        pocetobjektu++;
+
+        return b1.get(Box-1);
+    }
+
+    private Box prepareCube() {
+        Cube++;
+        System.out.println("PIS");
+        int x1 = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        String s = sc.next();
+
+        PhongMaterial material = new PhongMaterial();
+        //material.setDiffuseColor(Color.MIDNIGHTBLUE);
+        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/ffffff.png")));
+
+
+        b2.add(new Box(x1,x1,x1));
+        b2.get(Cube-1).setMaterial(material);
+
+        b2.get(Cube-1).translateXProperty().set(x);
+        b2.get(Cube-1).translateYProperty().set(y);
+        b2.get(Cube-1).translateZProperty().set(z);
+
+
+        chb[1].getItems().add(s);
+
+        cisla[k] = 3;
+        k++;
+        pocetobjektu++;
+
+        return b2.get(Cube-1);
+    }
+
+    public Cylinder prepareCylinder() {
+        Cylindr++;
+        System.out.println("PIS");
+        int x1 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        String s = sc.next();
+
+        PhongMaterial material = new PhongMaterial();
+        //material.setDiffuseColor(Color.MIDNIGHTBLUE);
+        material.setDiffuseMap(new Image(getClass().getResourceAsStream("/com/example/demo2/ffffff.png")));
+
+
+        cil.add(new Cylinder(x1,y2, 100));
+        cil.get(Cylindr-1).setMaterial(material);
+
+        cil.get(Cylindr-1).translateXProperty().set(x);
+        cil.get(Cylindr-1).translateYProperty().set(y);
+        cil.get(Cylindr-1).translateZProperty().set(z);
+
+
+        chb[1].getItems().add(s);
+
+        cisla[k] = 4;
+        k++;
+        pocetobjektu++;
+
+        return cil.get(Cylindr-1);
+
     }
 
 
-    private void initMouseControl(SmartGroup sg, Scene scene, Stage stage, BorderPane bp) {
+    private void initMouseControl(SmartGroup sg, Scene scene, Stage stage, BorderPane bp, VBox v) {
 
         Rotate xRotate;
         Rotate yRotate;
@@ -218,27 +473,32 @@ public class Camera3D extends Application {
 
         bp.setOnMousePressed(event -> {
 
-            anchorX = event.getSceneX();
-            anchorY = event.getSceneY();
+            anchorX = event.getScreenX();
+            anchorY = event.getScreenY();
             anchorAngleX = angleX.get();
             anchorAngleY = angleY.get();
 
         });
         bp.setOnMouseDragged(event -> {
             angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
-            angleY.set(anchorAngleY + (anchorX - event.getSceneX()));
+            angleY.set(anchorAngleY - (anchorX - event.getSceneX()));
         });
 
         bp.addEventHandler(ScrollEvent.SCROLL, event ->{
             double delta = event.getDeltaY();
-            sg.translateZProperty().set(sg.getTranslateZ() - delta);
+            sg.translateZProperty().set(sg.getTranslateZ() + 10);
+
 
         });
 
+
         bp.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
             switch (event.getCode()){
-                case UP: {
-                    sg.translateXProperty().set(sg.getTranslateX() + 10);
+                case W: {
+                    sg.translateZProperty().set(sg.getTranslateX() + 10);
+                }
+                case S: {
+                    sg.translateZProperty().set(sg.getTranslateX() - 10);
                 }
             }
         });
